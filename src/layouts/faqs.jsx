20e4dbd7b9af022355b1948beb09302a3f38d58b
rgoodie <rag@goodie>
2017-12-07 21:13:00 -0600

    importing fake data.

--- src/layouts/faqs.jsx
+++ src/layouts/faqs.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 
-const Faqs = () => (
-  <div className="faq">
-    <h2>FAQs</h2>
-  </div>
-);
+class Faqs extends React.Component {
 
-Faqs.test = () => {
-  console.log('test');
+  constructor(props) {
+    super(props)
+    this.state = {
+      question: []
+    }
+
+  }
+
+
+
+  componentDidMount() {
+    fetch('https://baconipsum.com/api/?type=meat-and-filler')
+      .then(r => r.json())
+      .then(data => {
+        this.setState({
+          question: data
+        })
+        console.dir(this.state)
+        return data
+      })
+      .catch(e => console.log("Fetch Failed"))
+
+
+  }
+
+  componentWillMount() {
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="faq">
+        <h2>FAQs</h2>
+        <ul>
+          {this.state.question.map((q, i) =>
+            <li key={i}>{q}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
-
-
 
 export default Faqs;
