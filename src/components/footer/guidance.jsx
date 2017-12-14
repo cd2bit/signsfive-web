5c9062d434502d91460404e7b04e0c5429a9b291
rgoodie <rag@goodie>
2017-12-14 08:38:50 -0600

    opps forgot to add new file

---
+++ src/components/footer/guidance.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FooterGuidance = () => (
+
+    <div className="col-md-2 tracked">
+        <div className="float-md-right text-md-left">
+            <h6 className="footer-type text-uppercase text-secondary">
+                Guidance
+   </h6>
+            <ul className="footer-type list-unstyled">
+                <li className="py-1">
+                    <a href="#" className="text-dark">
+                        How it works
+       </a>
+                </li>
+                <li className="py-1">
+                    <a href="#" className="text-dark">
+                        Help Center
+       </a>
+                </li>
+            </ul>
+        </div>
+    </div>)
+
+
+export default FooterGuidance
