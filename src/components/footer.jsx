22dd68f8730686067096b7947456d482abaa253d
rgoodie <rag@goodie>
2017-12-14 08:42:51 -0600

    moving footer social/connect block

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import FooterLogin from './footer/login';
 import FooterGuidance from './footer/guidance'
+import FooterConnect from './footer/connect'
 
 const Footer = () => (
   <footer className="bg-light">
@@ -59,19 +60,8 @@ const Footer = () => (
               </div>
             </div>
             {/* footer-banner-social */}
-            <div className="col-md-6">
-              <div className="float-md-right">
-              <a href="https://www.facebook.com/signsfive/" aria-label="Facebook">
-                <i className="fa fa-facebook fa-lg fa-inverse px-3"></i>
-              </a>
-              <a href="https://twitter.com/signsfive" aria-label="Twitter">
-                <i className="fa fa-twitter fa-lg fa-inverse px-3"></i>
-              </a>
-              <a href="https://www.instagram.com/signsfive" aria-label="Instagram">
-                <i className="fa fa-instagram fa-lg fa-inverse px-3"></i>
-              </a>
-            </div>
-          </div>
+            <FooterConnect/>
+
         </div>
       </div>
     </div>
---
+++ src/components/footer/connect.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FooterConnect = () => (
+
+
+    <div className="col-md-6">
+        <div className="float-md-right">
+            <a href="https://www.facebook.com/signsfive/" aria-label="Facebook">
+                <i className="fa fa-facebook fa-lg fa-inverse px-3"></i>
+            </a>
+            <a href="https://twitter.com/signsfive" aria-label="Twitter">
+                <i className="fa fa-twitter fa-lg fa-inverse px-3"></i>
+            </a>
+            <a href="https://www.instagram.com/signsfive" aria-label="Instagram">
+                <i className="fa fa-instagram fa-lg fa-inverse px-3"></i>
+            </a>
+        </div>
+    </div>
+
+)
+
+export default FooterConnect
