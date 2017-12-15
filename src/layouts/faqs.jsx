e158a9dcffe564e40fb4c0fffbdb246f39c568c2
rgoodie <rag@goodie>
2017-12-15 16:28:53 -0600

    corrected

--- src/layouts/faqs.jsx
+++ src/layouts/faqs.jsx
@@ -4,9 +4,7 @@ const Faqs = () => (
   <div className="faq">
     <h2>FAQs</h2>
     <ul>
-      {this.state.question.map((q, i) =>
-        <li key={i}>{q}</li>
-      )}
+     
     </ul>
   </div>
 )
