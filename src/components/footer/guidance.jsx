4d211cb40eada8acd1a48e52dc045846de3c51af
rgoodie <rag@goodie>
2017-12-16 10:58:53 -0600

    formating document

--- src/components/footer/guidance.jsx
+++ src/components/footer/guidance.jsx
@@ -6,19 +6,13 @@ const FooterGuidance = () => (
 
     <div className="col-md-2 tracked">
         <div className="float-md-right text-md-left">
-            <h6 className="footer-type text-uppercase text-secondary">
-                Guidance
-   </h6>
+            <h6 className="footer-type text-uppercase text-secondary">Guidance</h6>
             <ul className="footer-type list-unstyled">
                 <li className="py-1">
-                    <a href="#" className="text-dark">
-                        How it works
-       </a>
+                    <a href="#" className="text-dark">How it works</a>
                 </li>
                 <li className="py-1">
-                    <a href="#" className="text-dark">
-                        Help Center
-       </a>
+                    <a href="#" className="text-dark">Help Center</a>
                 </li>
             </ul>
         </div>
