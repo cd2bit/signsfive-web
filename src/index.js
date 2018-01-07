fd4e76cc2be35eb03502c408e926b07025e1327f
Giordon Stark <kratsg@gmail.com>
2018-01-06 18:06:27 -0600

    example login flow

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ import configureStore from './redux/configureStore';
 import App from './app';
 import registerServiceWorker from './utils/registerServiceWorker';
 
+import AuthService from './utils/AuthService';
+
+AuthService.handleAuthentication().then((profile) => {
+  console.log(profile);
+}).catch((err) => {
+  console.log(err);
+  if (!AuthService.isAuthenticated()) {
+    AuthService.login();
+  }
+});
+
 const store = configureStore();
 
 ReactDOM.render(
