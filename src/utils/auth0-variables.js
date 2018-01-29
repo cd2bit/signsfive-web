a02889cea5aa65eb40359b711f32a4748fb2124a
Giordon Stark <kratsg@gmail.com>
2018-01-29 08:55:57 +0100

    multiple auth0 configs

--- src/utils/auth0-variables.js
+++ src/utils/auth0-variables.js
@@ -1,7 +1,14 @@
 const AUTH_CONFIG = {
-  domain: 'signsfive.auth0.com',
-  clientId: 'MNZGv1cpercLQgy0fCb3MHqeO0HN8uGP',
-  callbackUrl: 'http://localhost:3000/',
+  development: {
+    domain: 'signsfive.auth0.com',
+    clientId: 'MNZGv1cpercLQgy0fCb3MHqeO0HN8uGP',
+    callbackUrl: 'http://localhost:3000/',
+  },
+  staging: {
+    domain: 'signsfive.auth0.com',
+    clientId: 'MNZGv1cpercLQgy0fCb3MHqeO0HN8uGP',
+    callbackUrl: 'https://signsfive-web-staging.herokuapp.com/',
+  },
 };
 
-export default AUTH_CONFIG;
+export default AUTH_CONFIG[process.env.NODE_ENV];
