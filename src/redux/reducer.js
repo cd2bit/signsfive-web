8af873913e2e950df88a0d85394037ecb2ee11ad
Jason Salerno <jason@meltedspork.com>
2018-01-28 23:23:03 -0600

    remove counter example

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './modules/auth';
-import counterReducer from './modules/counter';
 
 export default combineReducers({
   authReducer,
-  counterReducer,
 });
