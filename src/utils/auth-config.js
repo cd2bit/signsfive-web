488aed4e647815687c8e8c3fd3c1bbc0c1f73fee
Jason Salerno <jason@meltedspork.com>
2018-01-29 20:17:48 -0600

    Decoupled AUTH0 and AuthService. Also fixed tests.

--- src/utils/auth-config.js
+++ src/utils/auth-config.js
@@ -1,4 +1,9 @@
 const AUTH_CONFIG = {
+  test: {
+    domain: 'signsfive.auth0.test',
+    clientId: 'test_id',
+    callbackUrl: 'http://localhost:3000/',
+  },
   development: {
     domain: 'signsfive.auth0.com',
     clientId: 'MNZGv1cpercLQgy0fCb3MHqeO0HN8uGP',
