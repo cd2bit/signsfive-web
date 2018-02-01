cdc24f1e189bd633fb507ffad12d1d377383ad8b
Jason Salerno <jason@meltedspork.com>
2018-02-01 00:05:12 -0600

    add better logging for heroku for deployment

--- scripts/start.js
+++ scripts/start.js
@@ -4,6 +4,9 @@
 process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+console.log('Running ./scripts/start.js');
+console.log('NODE_ENV: ', process.env.NODE_ENV);
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
