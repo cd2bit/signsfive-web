369147f084c41716ae27608cb68cc8d06508ef21
Jason Salerno <jason@meltedspork.com>
2018-02-01 05:44:36 -0600

    removed unnecessary build log  and update homagepage with HTTPS

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,6 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
-console.log('Running ./scripts/build.js');
-
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
