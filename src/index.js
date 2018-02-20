a01b1c7d28df1832ca975efa2d103d1543e8b66c
Jason Salerno <jasons@jellyvision.com>
2018-02-20 00:21:44 -0600

    refactored react router and add route specs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import React from 'react';
  */
 import ReactDOM from 'react-dom';
 /**
+ * @external {react-router-dom} https://reacttraining.com/react-router/
+ */
+import { BrowserRouter } from 'react-router-dom';
+/**
  * @external {react-redux} https://redux.js.org/docs/basics/UsageWithReact.html
  */
 import { Provider } from 'react-redux';
@@ -23,9 +27,11 @@ const store = configureStore();
 
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter basename="/">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import App from '../src/app';
 import SearchBar from '../src/components/search-bar';
+
+import Main from '../src/components/main';
+import About from '../src/components/about';
+import Faqs from '../src/components/faqs';
 
 describe('<App />', () => {
   let wrapper;
@@ -12,5 +17,39 @@ describe('<App />', () => {
 
   it('renders <SearchBar />', () => {
     expect(wrapper.find(SearchBar)).to.have.lengthOf(1);
+  });
+
+  describe('renders correct routes', () => {
+    let route;
+    let routes;
+
+    beforeEach(() => {
+      routes = {};
+      wrapper.find(Route).forEach((routeMap) => {
+        const routeProps = routeMap.props();
+        routes[routeProps.path] = routeProps.component;
+      });
+    });
+
+    it('routes to Main', () => {
+      route = routes['/'];
+      expect(route).to.equal(Main);
+      expect(route).not.to.equal(About);
+      expect(route).not.to.equal(Faqs);
+    });
+
+    it('routes to About', () => {
+      route = routes['/about'];
+      expect(route).not.to.equal(Main);
+      expect(route).to.equal(About);
+      expect(route).not.to.equal(Faqs);
+    });
+
+    it('routes to Faqs', () => {
+      route = routes['/faqs'];
+      expect(route).not.to.equal(Main);
+      expect(route).not.to.equal(About);
+      expect(route).to.equal(Faqs);
+    });
   });
 });
