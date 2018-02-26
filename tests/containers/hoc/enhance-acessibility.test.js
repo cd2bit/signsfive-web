7108ee177b459175c62db106aae6ab478dac5e0e
Jason Salerno <jason@meltedspork.com>
2018-02-26 06:46:46 -0600

    no message

--- tests/containers/hoc/enhance-acessibility.test.js
+++ tests/containers/hoc/enhance-acessibility.test.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
 
-import AccessibleHOC from '../../../src/containers/hoc/enhance-accessibility';
+import withAccessible from '../../../src/containers/hoc/enhance-accessibility';
 
 describe('<AccessibleHOC />', () => {
   let MockComponent;
   let WrapperMockComponent;
   let wrapper;
+  let mockStore;
+  let store;
 
   beforeEach(() => {
-    MockComponent = () => (<div className="mockComponent" />); // eslint-disable-line react/jsx-filename-extension
-    WrapperMockComponent = AccessibleHOC(MockComponent);
-    wrapper = shallow(<WrapperMockComponent />);
+    mockStore = configureStore([thunk]);
+    store = mockStore({});
+    MockComponent = () => <div className="mockComponent" />; // eslint-disable-line react/jsx-filename-extension
+    WrapperMockComponent = withAccessible()(MockComponent);
+    wrapper = mount(<WrapperMockComponent store={store} />);
   });
 
+  // currently this is broken but i feel we are very close!
   xit('renders the wrapped component as the root element in the connect HOC', () => {
-    expect(wrapper.first()).to.equal(AccessibleHOC);
+    console.log('wrapperwrapperwrapperwrapper', wrapper.WrappedComponent);
+    expect(wrapper.find(withAccessible)).to.equal(withAccessible);
     expect(wrapper.first().first()).to.equal(MockComponent);
   });
 
-  xit('renders wrapped element', () => {
+  it('renders wrapped element', () => {
     expect(wrapper.find(MockComponent)).to.have.lengthOf(1);
   });
 });
